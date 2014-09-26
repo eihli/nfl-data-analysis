5e3105481ec7949f57821d100a489e55a74ab7a1
Eric Ihli <loktakwah@gmail.com>
2014-09-26 16:40:56 -0500

    Added function to get line movement urls

--- nfl3/scraper.py
+++ nfl3/scraper.py
@@ -1,4 +1,5 @@
 import urllib
+import re
 import json
 import urllib.parse
 import urllib.request
@@ -7,8 +8,9 @@ from bs4 import BeautifulSoup as bs
 class Scraper():
     
     def __init__(self):
-        self.base_url = "http://scores.covers.com" 
-        self.ajax_url = "http://scores.covers.com/ajax/SportsDirect.Controls.LiveScoresControls.Scoreboard,SportsDirect.Controls.LiveScoresControls.ashx?_method=UpdateScoreboard&_session=no"
+
+        self.select_week_url = "http://scores.covers.com/ajax/SportsDirect.Controls.LiveScoresControls.Scoreboard,SportsDirect.Controls.LiveScoresControls.ashx?_method=UpdateScoreboard&_session=no"
+        self.select_year_url= "http://scores.covers.com/ajax/SportsDirect.Controls.LiveScoresControls.ScoresCalendar,SportsDirect.Controls.LiveScoresControls.ashx?_method=changeDay&_session=no"
 
     def get_json_from_page(self, week, league, season, gamedate):
         str_html_year = ''
@@ -21,6 +23,25 @@ class Scraper():
                 'conferenceID': '',
         }
         data = urllib.parse.urlencode(data).replace('&','\n').replace('+',' ').encode('utf-8')
-        f = urllib.request.urlopen(self.ajax_url, data)
+        f = urllib.request.urlopen(self.select_week_url, data)
         json_data = f.read().decode('utf-8')
         return json_data
+    
+    def get_json_select_year(self, season_string):
+        data = {
+                'league': '1',
+                'SeasonString': season_string,
+                'Year': '1',
+                'Month': '1',
+                'Day': '1'
+        }
+        data = urllib.parse.urlencode(data).replace('&', '\n').encode('utf-8')
+        f = urllib.request.urlopen(self.select_year_url, data)
+        json_data = f.read().decode('utf-8')
+        return json_data
+
+    def get_line_move_urls(self, data):
+        p = re.compile(r'<a href="(http://www\.covers\.com/sports/odds/linehistory\.aspx.*?)".*?Line Moves')
+        urls = p.findall(data)
+        return urls
+
--- nfl3/tests/test_scraper.py
+++ nfl3/tests/test_scraper.py
@@ -1,15 +1,26 @@
 import unittest
 import urllib
-import scraper
+from .. import scraper
 from bs4 import BeautifulSoup as bs
 
 
 class TestScraper(unittest.TestCase):
 
     def setUp(self):
+        self.scraper = scraper.Scraper()
         pass
 
     def test_get_json_from_page(self):
-       self.scrape = scraper.Scraper()
-       json_data = self.scrape.get_json_from_page('Week 2', '1', '2013-2014', '9-12-2013')
+       json_data = self.scraper.get_json_from_page('Week 2', '1', '2013-2014', '9-12-2013')
        self.assertIn('2013', json_data)
+
+    def test_get_json_select_year(self):
+        json_data = self.scraper.get_json_select_year('2014-2015')
+        self.assertIn('HOF', json_data)
+
+    def test_get_line_move_urls(self):
+        json_data = self.scraper.get_json_from_page('Week 2', '1', '2013-2014', '9-12-2013')
+        line_move_urls = self.scraper.get_line_move_urls(json_data)
+        for line in line_move_urls:
+        self.assertEqual(len(line_move_urls), 16)
+        self.assertTrue('http://www.covers.com/sports/odds/linehistory.aspx?eventId=37608&amp;sport=nfl' in line_move_urls)
