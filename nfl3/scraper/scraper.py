f77322eafdc6f5736d74e323f87c0763c0ef9df7
Eric Ihli <loktakwah@gmail.com>
2014-09-29 21:07:04 -0500

    Huge update. Stored all covers html in shelf.
    Parsed a lot of what is needed.
    Need to get the odds for the point spread and over under.
    Need to clean up null/missing data.
    Almost ready to run a full parse.

--- nfl3/scraper/scraper.py
+++ nfl3/scraper/scraper.py
@@ -1,4 +1,6 @@
 import urllib
+import datetime
+import shelve
 import re
 import json
 import urllib.parse
@@ -100,23 +102,152 @@ class Scraper():
                     .replace('+', ' ').encode('utf-8')
             l_requests.append(urllib.request.Request(self.select_week_url, d_data))
         return l_requests
+    
+    def parse_line_movement_gamedate(html):
+        p = re.compile(r'h3LineHistory"><div class="right">(.*?)</div>', re.S)
+        result = p.search(html).group(1)
+        p = re.compile(r' ([0-9]):')
+        hour = p.search(result)
+        if hour.end(1) - hour.start(1) > 1:
+            result = result[:hour.start()+1] + 0 + result[hour.end():] 
+        result = datetime.datetime.strptime(result, '%a, %B %d/%y %I:%M %p ET')
+        return result
 
-    def get_list_of_line_move_links(self):
-        l_line_move_urls = []
-        l_year_requests = self.get_list_of_year_POST_requests()
-        for year_request in l_year_requests:
-            year_data = urllib.request.urlopen(year_request)\
-                .read().decode('utf-8')
-            l_week_requests = self.get_list_of_week_POST_requests(year_data)
-            for week_request in l_week_requests:
-                week_data = urllib.request.urlopen(week_request)\
-                    .read().decode('utf-8')
-                line_move_urls = self.parse_line_move_urls_from_data\
-                    (week_data)
-                l_line_move_urls += line_move_urls
-        f = open('line_move_links.txt', 'w')
-        for item in l_line_move_urls:
-            f.write("%s\n" % item)
-        f.close()
+    def parse_line_movement_teams(html):
+        html = ''.join((html.replace('\n', '').replace('\r','')).split())
+        p = re.compile(r'h3LineHistory.*?</div>(.*?)vs.(.*?)</h3>')
+        result = p.search(html)
+        result = [result.group(1), result.group(2)]
+        return result
+    
+    def parse_line_movement_sportsbooks(html):
+        p = re.compile(r'#ECECE4.*?underline;">(.*?)</a>(.*?)(#ECECE4|</table>)')
+        result = p.findall(html)
+        result = dict((key, value) for key, value, what in result)
+        return result
 
+    def parse_movement_datetime(html):
+        p = re.compile(r'<tr.*?<td.*?>(\d.*?)<')
+        p1 = re.compile(r' (\d):')
+        sportsbooks = Scraper.parse_line_movement_sportsbooks(html)
+        d_lines = {}
+        for book in sportsbooks:
+            d_lines[book] = []
+            results = p.findall(sportsbooks[book])
+            for line in results:
+                match = p1.search(line)
+                # Convert 1 digit hours to 2 digit hours
+                if match:
+                    line = line[:match.start() + 1] + '0' + line[match.end()-2:]
+                    
+                line = datetime.datetime.strptime(line, '%m/%d/%y %I:%M:%S %p')
+                d_lines[book].append(line)
+        return d_lines
+
+    def parse_point_spread(html):
+
+        p = re.compile(r'<tr bgcolor="#FFFFFF">(<td.*?</td>)' + \
+                '(<td.*?</td>)(<td.*?</td>)</tr>')
+        results = p.findall(html)
+        p = re.compile(r'((-\d*?)|(\d*?))/|>(([A-Z])*?<)')
+
+        for line in results:
+            point_spread = p.search(line[1]).group(1)
+    
+    def parse_line_movements(html):
+
+        date = Scraper.parse_line_movement_gamedate(html)
+        home_team, away_team = Scraper.parse_line_movement_teams(html)
+
+        re_table = re.compile(r'<table id="ucLineHistory_tblLineHistory".*?</table>')
+        
+        table = re_table.search(html).group(0)
+
+        re_sportsbooks = re.compile(r'<td class="smtext2" width="40%">(.*?)(tr bgcolor="#ECECE4">|</table>)')
+        re_sportsbook_name = re.compile(r'Covers Line History".*?>(.*?)<')
+
+        sportsbook_names = re_sportsbook_name.findall(table)
+        sportsbooks_table = re_sportsbooks.findall(table)
+        d_sportsbooks = dict(zip(sportsbook_names, sportsbooks_table))
+
+        re_line_movement = re.compile(r"""
+            <tr\ bgcolor="\#FFFFFF"> # Row...
+            .*?>(.*?)</td>           # First column
+            .*?>(.*?)</td>           # Second column
+            .*?>(.*?)</td>           # Third columnd
+            """, re.X)
+        re_hour_digit = re.compile(r' (\d*?):')
+        re_point_spread = re.compile(r'(.*?)/')
+        re_over_under_half_point = re.compile(r'..\..')
+        re_over_under = re.compile(r'..')
+
+        l_result = []
+        
+        for bookname in d_sportsbooks.keys():
+            print(bookname)
+
+            # The values in sportsbooks are tuples because it's the only way
+            # you could get regex to work. value in [0] index is the
+            # one you want
+            l_line_movements = re_line_movement.findall(d_sportsbooks[bookname][0])
+
+            for line_movement in l_line_movements:
+
+                # Format the datetime
+                dt_line_movement = line_movement[0]
+                hour = re_hour_digit.search(dt_line_movement)
+                if len(hour.group(1)) == 1:
+                    dt_line_movement = dt_line_movement[:hour.start() + 1]\
+                          + '0' + dt_line_movement[hour.end() - 2:]
+                dt_line_movement = datetime.datetime.strptime(dt_line_movement,
+                        '%m/%d/%y %I:%M:%S %p')
+
+                # Parse and format the point spread
+                point_spread = line_movement[1]
+                point_spread = re_point_spread.search(point_spread)
+                if not point_spread:
+                    point_spread = 'OFF'
+                else:
+                    point_spread = point_spread.group(1)
+                
+                # TODO: Parse and format point spread ODDS
+
+                # Parse and format Over Under
+                over_under = line_movement[2]
+                if re_over_under_half_point.search(over_under):
+                    over_under = re_over_under_half_point.search(over_under)
+                else:
+                    over_under = re_over_under.search(over_under)
+                over_under = over_under.group(0)
+
+                l_result.append((bookname, dt_line_movement, point_spread,
+                    over_under))
+        print(len(l_result))
+        return l_result
+
+
+#    def get_list_of_line_move_links(self):
+#        l_line_move_urls = []
+#        l_year_requests = self.get_list_of_year_POST_requests()
+#        for year_request in l_year_requests:
+#            year_data = urllib.request.urlopen(year_request)\
+#                .read().decode('utf-8')
+#            l_week_requests = self.get_list_of_week_POST_requests(year_data)
+#            for week_request in l_week_requests:
+#                week_data = urllib.request.urlopen(week_request)\
+#                    .read().decode('utf-8')
+#                line_move_urls = self.parse_line_move_urls_from_data\
+#                    (week_data)
+#                l_line_move_urls += line_move_urls
+#        f = open('line_move_links.txt', 'w')
+#        for item in l_line_move_urls:
+#            f.write("%s\n" % item)
+#        f.close()
+
+#    def get_and_shelve_line_movement_html(self, link_list, shelf):
+#        for link in link_list:
+#            key = link
+#            value = urllib.request.urlopen(link).read()
+#            shelf[key] = value
+#        return
 
--- nfl3/scraper/test_scraper.py
+++ nfl3/scraper/test_scraper.py
@@ -1,3 +1,7 @@
+import re
+import datetime
+import os
+import shelve
 import unittest
 import urllib
 import urllib.request
@@ -8,6 +12,15 @@ class TestScraper(unittest.TestCase):
 
     def setUp(self):
         self.scraper = scraper.Scraper()
+        self.d = shelve.open('line_movement_shelf')
+        self.f = open('new_line_move_links.txt')
+        self.link_list = [link.strip() for link in self.f]
+        self.f.close()
+        self.test_parse_html = self.d[self.link_list[0]].decode('utf-8').\
+                replace('\n', '').replace('\r', '').replace('\t', '')
+
+    def tearDown(self):
+        self.d.close()
 
     def test_get_data_for_week(self):
        json_data = self.scraper.get_data_for_week('Week 2', '1', '2013-2014', '9-12-2013')
@@ -43,17 +56,72 @@ class TestScraper(unittest.TestCase):
         self.assertTrue(len(html) > 2000)
         self.assertIn('league', l_POST_requests[2].data.decode('utf-8'))
 
-    def test_get_list_of_week_POST_requests(self):
-        year_data = self.scraper.get_data_for_year('2014-2015')
-        l_POST_requests = self.scraper.get_list_of_week_POST_requests(year_data)
-        req = l_POST_requests[0]
-        f = urllib.request.urlopen(req)
-        html = f.read().decode('utf-8')
-        self.assertIn('Line Moves', html)
-        self.assertTrue(len(html) > 2000)
-        self.assertIn('LeagueID', l_POST_requests[2].data.decode('utf-8'))
+    def test_parse_line_movement_html(self):
+        for i in range(5):
+            html = self.d[self.link_list[i]]
 
-    def test_get_list_of_line_move_links(self):
-        l_line_move_links = self.scraper.get_list_of_line_move_links()
-        self.assertTrue(len(l_line_move_links) > 40)
-        self.fail("Add better testing")
+    def test_parse_line_movement_game_date(self):
+        html = self.d[self.link_list[0]].decode('utf-8')
+        result = scraper.Scraper.parse_line_movement_game_date(html)
+        self.assertEqual(result.hour, datetime.time(hour=20).hour)
+
+    def test_parse_line_movement_teams(self):
+        html = self.d[self.link_list[0]].decode('utf-8')
+        result = scraper.Scraper.parse_line_movement_teams(html)
+        self.assertEqual(result[0], 'Miami')
+        self.assertEqual(result[1], 'Dallas')
+
+    def test_parse_line_movement_sportsbooks(self):
+        result = scraper.Scraper.parse_line_movement_sportsbooks(self.\
+                test_parse_html)
+        self.assertTrue(any('5Dimes.eu' in i for i in result))
+        self.assertTrue('07/25/13' in result['5Dimes.eu'])
+
+    def test_parse_movement_datetime(self):
+        result = scraper.Scraper.parse_movement_datetime(self.\
+                test_parse_html)
+        self.assertTrue(result['5Dimes.eu'][0].hour == 9)
+
+    def test_parse_point_spread(self):
+        result = scraper.Scraper.parse_point_spread(self.test_parse_html)
+        self.fail('Add a descriptive test here')
+
+    def test_parse_line_movements(self):
+        result = scraper.Scraper.parse_line_movements(self.test_parse_html)
+        self.fail('Add test dumbass stop being lazy')
+
+
+#    def test_get_list_of_week_POST_requests(self):
+#        year_data = self.scraper.get_data_for_year('2014-2015')
+#        l_POST_requests = self.scraper.get_list_of_week_POST_requests(year_data)
+#        req = l_POST_requests[0]
+#        f = urllib.request.urlopen(req)
+#        html = f.read().decode('utf-8')
+#        self.assertIn('Line Moves', html)
+#        self.assertTrue(len(html) > 2000)
+#        self.assertIn('LeagueID', l_POST_requests[2].data.decode('utf-8'))
+
+#    def test_get_list_of_line_move_links(self):
+#        l_line_move_links = self.scraper.get_list_of_line_move_links()
+#        self.assertTrue(len(l_line_move_links) > 40)
+
+#    def test_get_and_shelve_line_movement_html(self):
+#        link_list = ['http://www.covers.com/sports/odds/linehistory.aspx?eventId=37575&sport=nfl']
+#        shelf = shelve.open('line_movements_shelf', 'r')
+#        self.scraper.get_and_shelve_line_movement_html(link_list, shelf)
+#        self.assertTrue(os.path.isfile('line_movements_shelf.dat'))
+#        test_html = """
+#    <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+#    <HTML>
+#        <HEAD>
+#            
+#
+#    <!-- Start Meta Data -->
+#    <meta http-equiv="X-UA-Compatible" value="IE=9">
+#    <TITLE>NFL Line History - Football Betting Odds</TITLE>
+#    <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+#"""
+#        test_html = "".join(test_html.split())
+#        self.assertIn(test_html, "".join(shelf[link_list[0]].decode('utf-8').split()))
+#        shelf.close()
+#        self.fail("You just made sure things were writing to the shelf. Now try writing multiple pages to the shelf. Once you get multiple pages writing and retrieving from shelf, run it on all the links in the list")
